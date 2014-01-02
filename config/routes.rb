7e0b06de1f124e7497b3fed88430dbe5ae62500e
Adam Jacox <yellowjacox@gmail.com>
2014-01-02 14:14:28 -0700

    Add GET admin/logout route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Shadmin::Application.routes.draw do
-  devise_for :admins, path: 'admin'
+  devise_for :admins, path: 'admin', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
+  devise_scope :admin do
+    get 'admin/logout', to: 'devise/sessions#destroy', via: :delete
+  end
   # root 'welcome#index'
 end
