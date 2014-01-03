3db1eb945749a14d3cc14f80d3a5665a753cee60
Adam Jacox <yellowjacox@gmail.com>
2014-01-02 17:11:35 -0700

    Remove devise registerable

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Shadmin::Application.routes.draw do
-  devise_for :admins, path: 'admin', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
+  devise_for :admins, path: 'admin', path_names: { sign_in: 'login', sign_out: 'logout' }
   devise_scope :admin do
     get 'admin/logout', to: 'devise/sessions#destroy', via: :delete
   end
