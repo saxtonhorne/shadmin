441017c0b8b6d32dac07168c45f3a289604d2af5
Adam Jacox <yellowjacox@gmail.com>
2014-01-03 10:15:35 -0700

    Test admin email uniqueness

--- test/models/admin_test.rb
+++ test/models/admin_test.rb
@@ -10,17 +10,26 @@ class AdminTest < ActiveSupport::TestCase
   end
 
   def test_validates_email
+  	assert admin.valid?
   	admin.email = 'peter'
   	assert_not admin.valid?
   end
 
   def test_validates_password_length_min
+  	assert admin.valid?
   	admin.password = 'a' * 7
   	assert_not admin.valid?
   end
 
   def test_validates_password_length_max
+  	assert admin.valid?
   	admin.password = 'a' * 129
+  	assert_not admin.valid?
+  end
+
+  def test_validates_email_uniqueness
+  	assert admin.valid?
+  	admin.email = admins(:bob).email
   	assert_not admin.valid?
   end
 end
