5ccf1ce416e14963408484067ad092d7af87483e
Adam Jacox <yellowjacox@gmail.com>
2014-01-03 17:04:23 -0700

    Basic markup to display admin_user data on front-end

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Shadmin::Application.routes.draw do
+
+  root to: redirect('/admin/admin_users')
+
+  get '/admin', to: redirect('/admin/login')
+
+  # Devise routes
   devise_for :admins, path: 'admin', path_names: { sign_in: 'login', sign_out: 'logout' }
   devise_scope :admin do
     get 'admin/logout', to: 'devise/sessions#destroy', via: :delete
   end
-
-  # root to: 'admin#index'
 
   namespace :admin do
     resources :admin_users
