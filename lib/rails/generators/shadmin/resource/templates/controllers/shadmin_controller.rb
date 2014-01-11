d65ecfc3f879f14ce891c0f64a29b13311a04366
Adam Jacox <yellowjacox@gmail.com>
2014-01-10 17:15:38 -0700

    Include all restful actions in controller template

--- lib/rails/generators/shadmin/resource/templates/controllers/shadmin_controller.rb
+++ lib/rails/generators/shadmin/resource/templates/controllers/shadmin_controller.rb
@@ -1,6 +1,24 @@
 module Admin
 	class <%= resource_name.classify %>Controller < Shadmin::ApplicationController
-		# def index
-		# end
+		def index
+		end
+
+		def show
+		end
+
+		def new
+		end
+
+		def create
+		end
+
+		def edit
+		end
+
+		def update
+		end
+
+		def destroy
+		end
 	end
 end
