bf798f8c26a7143a9b3215c4531f8db6bb862485
Adam Jacox <yellowjacox@gmail.com>
2014-01-30 14:02:49 -0700

    Assert correct views are rendered for passwords_controller actions

--- test/controllers/shadmin/passwords_controller_test.rb
+++ test/controllers/shadmin/passwords_controller_test.rb
@@ -16,9 +16,9 @@ module Shadmin
 		end
 
 		def test_edit_action_template_layout
-			sign_in @admin
-			get :edit, use_route: :shadmin
-			# assert_response :success
+			sign_out @admin
+			get :edit, { reset_password_token: 'asgaseg' }, use_route: :shadmin
+			assert_response :success
 			assert_template layout: 'shadmin/sessions'
 			assert_template 'shadmin/passwords/edit'
 		end
